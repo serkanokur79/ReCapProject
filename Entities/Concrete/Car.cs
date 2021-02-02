f0fcaba65155b88d685c6d93de18924a277dfb32
Serkan Okur <serkan.okur@gmail.com>
2021-02-02 11:55:41 +0100

    IVehicle renamed to IEntity

--- Entities/Concrete/Car.cs
+++ Entities/Concrete/Car.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Entities.Concrete
 {
-    public class Car:IVehicle
+    public class Car:IEntity
     {
         public int Id { get; set; }
         public int BrandId { get; set; }
