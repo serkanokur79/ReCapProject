8367da4cfb1d620f4fe820b1dafb3d6563db9dbe
Serkan Okur <serkan.okur@gmail.com>
2021-02-05 23:22:16 +0100

    Entity props are same with Database

--- Entities/Concrete/Brand.cs
+++ Entities/Concrete/Brand.cs
@@ -6,7 +6,6 @@ namespace Entities.Concrete
 {
     public class Brand:IEntity
     {
-        public int Id { get; set; }
         public int BrandId { get; set; }
         public string BrandName { get; set; }
     }
--- Entities/Concrete/Color.cs
+++ Entities/Concrete/Color.cs
@@ -6,7 +6,6 @@ namespace Entities.Concrete
 {
     public class Color:IEntity
     {
-        public int Id { get; set; }
         public int ColorId { get; set; }
         public string ColorName { get; set; }
 
