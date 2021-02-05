b757c0a24c91428fbe946b7813d94fe0f7a059f8
Serkan Okur <serkan.okur@gmail.com>
2021-02-05 22:15:52 +0100

    GetById is renamed to Get

--- DataAccess/Abstract/IEntityRepository.cs
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -9,7 +9,7 @@ namespace DataAccess.Abstract
     public interface IEntityRepository<T> where T:class, IEntity, new()
     {
         List<T> GetAll(Expression<Func<T,bool>> filter = null);
-        T GetById(Expression<Func<T, bool>> filter);
+        T Get(Expression<Func<T, bool>> filter);
         void Add(T entity);
         void Update(T entity);
         void Delete(T entity);
