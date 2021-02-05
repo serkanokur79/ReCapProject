d872efdac9ffded7627ea4c2014cdab35b25bf25
Serkan Okur <serkan.okur@gmail.com>
2021-02-06 00:59:28 +0100

    Car Entity updated

--- Entities/Concrete/Car.cs
+++ Entities/Concrete/Car.cs
@@ -9,8 +9,8 @@ namespace Entities.Concrete
         public int Id { get; set; }
         public int BrandId { get; set; }
         public int ColorId { get; set; }
-        public DateTime ModelYear { get; set; }
-        public double DailyPrice { get; set; }
+        public int ModelYear { get; set; }
+        public decimal DailyPrice { get; set; }
         public string Description { get; set; }
     }
 }
